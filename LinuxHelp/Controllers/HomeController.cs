be8342a73efea2045f26886d55076d65288a76f3
Maksim Gorbachewski <gorbachewski.m@gmail.com>
2021-11-05 17:17:47 +0300

    v1.0

---
+++ LinuxHelp/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using LinuxHelp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LinuxHelp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            //ViewBag
+            return View();
+        }
+
+        // Обработка отправки формы (POST-запрос)
+        [HttpPost]
+        public IActionResult Index(SearchCommand info)
+        {
+            if (ModelState.IsValid)
+            {
+                // get info
+                return Redirect("https://tldr.ostera.io/"+info.Command);
+            }
+            else
+                return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ LinuxHelp/Models/SearchCommand.cs
@@ -0,0 +1,11 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace LinuxHelp.Models
+{
+    public class SearchCommand
+    {
+        [Required(ErrorMessage = "Пожалуйста, введите команду")]
+        public string Command { get; set;}
+    }
+}
